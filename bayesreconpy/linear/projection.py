12e200b84fc69f941f490d1d0f051db2d39b23f9
Anubhabsupsi <anubhab.biswas@supsi.ch>
2026-05-11 18:34:23 +0200

    minor change for linear projection.py

--- bayesreconpy/linear/projection.py
+++ bayesreconpy/linear/projection.py
@@ -1,6 +1,6 @@
 """Projection-based linear reconciliation methods."""
 
-from bayesreconpy.reconc_ols import get_S_from_A, reconc_ols
-from bayesreconpy.reconc_mint import estimate_cov_matrix, reconc_mint
+from bayesreconpy.reconc_ols import reconc_ols
+from bayesreconpy.reconc_mint import reconc_mint
 
-__all__ = ["get_S_from_A", "reconc_ols", "estimate_cov_matrix", "reconc_mint"]
+__all__ = ["reconc_ols", "reconc_mint"]
